fb0b46a2b894cdea43eebd929e5ed804a90af819
MinseongKimz <minseongkimc@gmail.com>
2022-04-11 17:55:36 +0900

    2022-04-11 수업내용 for WEB

---
+++ WebApp04/src/com/test/Test004.java
@@ -0,0 +1,96 @@
+/*=============================
+ * 		Test004.java
+ *  	 - Servlet 실습
+ * ===========================*/
+
+// 현재.. 자바의 기본 클래스 Test004
+// 이를 Servlet 으로 구성하는 방법.
+
+// GenericServlet 을 상속받는 클래스로 설계 → Servlet
+
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Test004 extends GenericServlet // GenericServlet은 추상 클래스여서 상속받으려면 service메소드를 오버라이드 해줘야함.
+{
+
+	private static final long serialVersionUID = 1L;
+
+	//GenericServlet 의 추상메소드 service 추상메소드 재정의
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/html; charset=UTF-8");
+		
+		try
+		{
+			// 출력 스트림
+			PrintWriter out = response.getWriter();
+			
+			out.print("<html>");
+			
+			out.print("<head>");
+			out.print("</head>");
+			
+			out.print("<title>");
+			out.print("Test004.java");
+			out.print("</title>");
+			
+			out.print("<body>");
+			
+			out.print("<div>");
+			out.print("<h1>");
+			out.print("서블릿 관련 실습");
+			out.print("<hr>");
+			out.print("</h1>");
+			out.print("</div>");
+			
+			out.print("<div>");
+			out.print("<h2>");
+			out.print("GenericServlet 클래스를 이용한 서블릿 테스트");
+			out.print("</h2>");
+			out.print("</div>");
+			
+			
+			out.print("</body>");
+			
+			out.print("</html>");
+			
+		} catch (Exception e)
+		{
+			// TODO: handle exception
+		}
+		
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
